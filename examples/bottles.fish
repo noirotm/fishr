db475cafad5f3ef56bf6d919c9c8f82c7a4d88bd
Marc Noirot <marc.noirot@gmail.com>
2017-03-02 10:23:01 +0100

    Add more examples

---
+++ examples/bottles.fish
@@ -0,0 +1,7 @@
+4b9**1-&    v         >&::&4%:?!v4$-+v       >" bottle" v                                                            >", "v
+    >&:&0(?;>&:&4%1=?;^         >~   >4,:n1=?^" bottles">" of beer">&1-:&4%1=                ?v" on the wall"&:&4%2=?^"."a>rv
+ l?!^o02.                            >                                                                                      >
+                                     ^r" ,dnuora ti ssap ,nwod eno ekaT"          v?=0,4&:-1&a<
+                                              vr" ,erom emos yub ,erots eht ot oG"<
+                                              > l?!vof3*1+5.
+                                                   >4b9**faa++1.
