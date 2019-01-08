ce780a77a530d6f0c69fd5d3cb4c18d13770c4a8
Marc Noirot <marc.noirot@gmail.com>
2019-01-08 22:36:56 +0100

    Implement `From' for Val

--- src/val.rs
+++ src/val.rs
@@ -57,6 +57,42 @@ impl Val {
     }
 }
 
+impl From<u8> for Val {
+    fn from(v: u8) -> Self {
+        Val::Byte(v)
+    }
+}
+
+impl From<i64> for Val {
+    fn from(v: i64) -> Self {
+        Val::Int(v)
+    }
+}
+
+impl From<f64> for Val {
+    fn from(v: f64) -> Self {
+        Val::Float(v)
+    }
+}
+
+impl From<Val> for u8 {
+    fn from(v: Val) -> Self {
+        v.to_u8()
+    }
+}
+
+impl From<Val> for i64 {
+    fn from(v: Val) -> Self {
+        v.to_i64()
+    }
+}
+
+impl From<Val> for f64 {
+    fn from(v: Val) -> Self {
+        v.to_f64()
+    }
+}
+
 impl PartialEq for Val {
     fn eq(&self, other: &Self) -> bool {
         match (*self, *other) {
@@ -103,6 +139,49 @@ mod tests {
     fn val_to_f64_works() {
         let val = Val::Byte(15);
         assert_eq!(val.to_f64(), 15.0);
+    }
+
+    #[test]
+    fn from_works() {
+        assert_eq!(Val::from(b'a'), Val::Byte(b'a'));
+        assert_eq!(Val::from(-512i64), Val::Int(-512));
+        assert_eq!(Val::from(3.14), Val::Float(3.14));
+    }
+
+    #[test]
+    fn generated_into_works() {
+        let val: Val = b'a'.into();
+        assert_eq!(val, Val::Byte(b'a'));
+
+        let val: Val = (-512i64).into();
+        assert_eq!(val, Val::Int(-512));
+
+        let val: Val = 3.14.into();
+        assert_eq!(val, Val::Float(3.14));
+    }
+
+    #[test]
+    fn from_val_works() {
+        let v = u8::from(Val::Byte(b'a'));
+        assert_eq!(v, b'a');
+
+        let v = i64::from(Val::Int(-512));
+        assert_eq!(v, -512);
+
+        let v = f64::from(Val::Float(3.14));
+        assert_eq!(v, 3.14);
+    }
+
+    #[test]
+    fn generated_val_into_works() {
+        let v: u8 = Val::Byte(b'a').into();
+        assert_eq!(v, b'a');
+
+        let v: i64 = Val::Int(-512).into();
+        assert_eq!(v, -512);
+
+        let v: f64 = Val::Float(3.14).into();
+        assert_eq!(v, 3.14);
     }
 
     #[test]
